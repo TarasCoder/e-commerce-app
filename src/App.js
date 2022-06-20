e7fa5a2f08c4e0e2d2e77bcc80d2bdcbe866fd8d
unknown <4kaskad@gmail.com>
2022-06-20 23:46:00 +0300

    adding firebase to project and sign in link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./routes/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/Navigation";
+import SignIn from "./routes/Sign-in/SignIn";
 
 const Shop = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
